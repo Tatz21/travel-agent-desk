88d48a79a99a05d4820b7515c5cfc56f6fb2ce7e
phoenixrealesthaticbusiness <phoenixrealesthaticbusiness@gmail.com>
2025-12-03 12:14:46 +0530

    Update sender_id and message values in SMS OTP function

--- supabase/functions/sms-otp/index.ts
+++ supabase/functions/sms-otp/index.ts
@@ -71,8 +71,8 @@ serve(async (req) => {
         },
         body: JSON.stringify({
           route: 'dlt',
-          sender_id: 'PHEWAN',
-          message: 204304,
+          sender_id: 'BANMSF',
+          message: 203806,
           variables_values: `${generatedOtp}|${validTime}`,
           language: 'english',
           flash: 0,
