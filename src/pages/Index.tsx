03ac12b8d35bf1529d57b02c35fcf70e32d8caa4
phoenixrealesthaticbusiness <phoenixrealesthaticbusiness@gmail.com>
2026-01-04 22:27:41 +0530

    Change navigation from '/setup' to '/register'

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -31,7 +31,7 @@ const Index = () => {
         if (agent) {
           navigate('/dashboard');
         } else {
-          navigate('/setup');
+          navigate('/register');
         }
       }
     }
