1d93203a6d71ae0b2fb35115afed7f02a0555acc
phoenixrealesthaticbusiness <phoenixrealesthaticbusiness@gmail.com>
2026-01-14 14:24:53 +0530

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Calendar, User, ArrowRight } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
