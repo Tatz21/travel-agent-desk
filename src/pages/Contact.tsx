2bc71539f5dbe7640cd3b48c6e55c1cfb09c69b1
phoenixrealesthaticbusiness <phoenixrealesthaticbusiness@gmail.com>
2026-01-24 03:15:11 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,7 +9,7 @@ import { useToast } from "@/hooks/use-toast";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { supabase } from "@/integrations/supabase/client";
-import heroContactImage from "@/assets/contact-us.jpg";
+import heroContactImage from "@/assets/contactus.png";
 
 const contactInfo = [
   {
