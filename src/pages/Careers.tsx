722659b1316137c7f7dddb3d3f88a90cbb049b33
phoenixrealesthaticbusiness <phoenixrealesthaticbusiness@gmail.com>
2026-01-27 16:51:06 +0530

    Update Careers.tsx

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -118,7 +118,12 @@ const Careers = () => {
       <Header />
       <section className="relative overflow-hidden">
         {/* HERO CONTAINER */}
-        <div className="relative w-full">
+        <div className="relative w-full
+          h-[45vh]
+          sm:h-[55vh]
+          md:h-[65vh]
+          lg:h-[75vh]
+          xl:h-[85vh]">
           {/* Mobile Image (NO CROPPING) */}
           <img
             src={heroCareerImage}
