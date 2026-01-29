09b6d3a0c88d8c83dcdae465db7b648df6c7f5c9
phoenixrealesthaticbusiness <phoenixrealesthaticbusiness@gmail.com>
2026-01-29 11:51:16 +0530

    Update IdleTimerProvider.tsx

--- src/components/IdleTimerProvider.tsx
+++ src/components/IdleTimerProvider.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useAuth } from "@/components/AuthProvider";
 
-const IDLE_TIME = 10 * 60 * 1000; // 10 minutes
+const IDLE_TIME = 50 * 60 * 1000; // 10 minutes
 const WARNING_TIME = 30; // 30 seconds
 
 export const IdleTimerProvider = ({
@@ -129,5 +129,6 @@ export const IdleTimerProvider = ({
     </>
   );
 };
+
 
 
